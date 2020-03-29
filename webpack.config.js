e2974be04d9c7f46fbeedcaf123ae5e3cc4b7981
Maksim Slyunkov <slyunkovmax@gmail.com>
2020-03-29 21:00:28 +0300

    add PostCSS and hash function

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,38 @@
 const path = require('path'); // подключаем path к конфигу вебпак
 const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Подключили к проекту плагин
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackMd5Hash = require('webpack-md5-hash');
 
 module.exports = {
     entry: { main: './src/index.js' },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js'
+        filename: '[name].[chunkhash].js'
     },
     module: {
         rules: [
             {
                 test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+                exclude: /node_modules/, // исключает папку node_modules
                 use: {
                     loader: "babel-loader" // весь JS обрабатывается пакетом babel-loader
-                },
-                exclude: /node_modules/ // исключает папку node_modules
+                }
             },
             {
                 test: /\.css$/, // применять это правило только к CSS-файлам
-                use: [MiniCssExtractPlugin.loader, 'css-loader'] // к этим файлам нужно применить пакеты, которые мы уже установили
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'] // добавили минификацию CSS
             }
         ]
     },
     plugins: [ 
         new MiniCssExtractPlugin({
-            filename: 'style.css'
+            filename: 'style.[contenthash].css'
         }),
         new HtmlWebpackPlugin({
             inject: false, // стили НЕ нужно прописывать внутри тегов
-            hash: true, // для страницы нужно считать хеш
             template: './index.html', // откуда брать образец для сравнения с текущим видом проекта
             filename: 'index.html' // имя выходного файла, то есть того, что окажется в папке dist после сборки
-          })
+        }),
+        new WebpackMd5Hash()
     ]
-}
+};
